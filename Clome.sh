bfb016f2c0a304020ecbc8d2215e8340671927e4
BeeepBoooop <84643406+BeeepBoooop@users.noreply.github.com>
2023-03-23 01:54:36 +0700

    Update path
    Reconfigure pathing to suit the cloned repo

--- Clome.sh
+++ Clome.sh
@@ -1,5 +1,6 @@
+    #!/bin/bash
     bash 0-preinstall.sh
-    arch-chroot /mnt /root/Clome/1-setup.sh
-    source /mnt/root/Clome/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/Clome/2-user.sh
-    arch-chroot /mnt /root/Clome/3-post-setup.sh
+    arch-chroot /mnt /root/Archh/1-setup.sh
+    source /mnt/root/Archh/install.conf
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/Archh/2-user.sh
+    arch-chroot /mnt /root/Archh/3-post-setup.sh
