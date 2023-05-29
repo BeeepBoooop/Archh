4a25b10393b59388c7a5029f66eeade971691b4c
BeeepBoooop <84643406+BeeepBoooop@users.noreply.github.com>
2023-05-29 12:24:19 +0700

    Update Clome.sh
    Resorting scripts to discrete folder, minimize conflict with later patch

--- Clome.sh
+++ Clome.sh
@@ -1,6 +1,47 @@
-    #!/bin/bash
-    bash 0-preinstall.sh
-    arch-chroot /mnt /root/Archh/1-setup.sh
-    source /mnt/root/Archh/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/Archh/2-user.sh
-    arch-chroot /mnt /root/Archh/3-post-setup.sh
+#!/bin/bash
+set -a
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts
+CONFIGS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/configs
+set +a
+echo -ne 
+"
+-------------------------------------------------------------------------
+ #####                              
+#     # #       ####  #    # ###### 
+#       #      #    # ##  ## #      
+#       #      #    # # ## # #####  
+#       #      #    # #    # #      
+#     # #      #    # #    # #      
+ #####  ######  ####  #    # ###### 
+                                    
+-------------------------------------------------------------------------
+                        AIO Arch Linux Installer
+-------------------------------------------------------------------------
+                Script are located in directory named Clome
+"   
+    ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
+      source $CONFIGS_DIR/setup.conf
+    ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
+    ( arch-chroot /mnt $HOME/Clome/scripts/1-setup.sh )|& tee 1-setup.log
+    if [[ ! $DESKTOP_ENV == server ]]; then
+      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/Clome/scripts/2-user.sh )|& tee 2-user.log
+    fi
+    ( arch-chroot /mnt $HOME/Clome/scripts/3-post-setup.sh )|& tee 3-post-setup.log
+    cp -v *.log /mnt/home/$USERNAME
+
+echo -ne " 
+-------------------------------------------------------------------------
+ #####                              
+#     # #       ####  #    # ###### 
+#       #      #    # ##  ## #      
+#       #      #    # # ## # #####  
+#       #      #    # #    # #      
+#     # #      #    # #    # #      
+ #####  ######  ####  #    # ###### 
+                                    
+-------------------------------------------------------------------------
+                        AIO Arch Linux Installer
+-------------------------------------------------------------------------
+            Installation complete - Eject Install Media and Reboot 
+" 
